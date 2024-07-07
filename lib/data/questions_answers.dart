7ef8a8018465e40f4769e0eab4d3b7e1967e90c0
Han-Websterr <han.o828.m104@gmail.com>
2024-07-07 19:08:53 +0900

    Add questions and answer

---
+++ lib/data/questions_answers.dart
@@ -0,0 +1,24 @@
+import 'package:adv_basics/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    "What muscle group do squats primarily target?",
+    ["Quadriceps", "Biceps", "Triceps", "Deltoids"]
+  ),
+  QuizQuestion(
+    "Which exercise is best for building chest muscles?", 
+    ["Bench Press", "Deadlift", "Leg Press", "Shoulder Press"]
+  ),
+  QuizQuestion(
+    "What is the main benefit of doing deadlifts?", 
+    ["Build lower body strength", "Improve flexibility", "Enhance cardiovascular health", "Increase grip strength"]
+  ),
+  QuizQuestion(
+    "Which muscle group is primarily worked during a pull-up?", 
+    ["Back", "Chest", "Legs", "Abs"]
+  ),
+  QuizQuestion(
+    "How many major muscle groups are there in the human body?", 
+    ["8", "6", "10", "12"]
+  ),
+];
--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,4 +1,6 @@
+import 'package:adv_basics/answer_button.dart';
 import 'package:flutter/material.dart';
+import 'package:adv_basics/data/questions_answers.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -12,6 +14,30 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(context) {
-    return const Center(child: Text('Question Screen'));
+    final currentQuestion = questions[0];
+    
+    return Container(
+      margin: const EdgeInsets.all(40),
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.answers.map((answer) {
+              return AnswerButton(answerText: answer, onTap: () {});
+            }),
+          ],
+        ),
+      ),
+    );
   }
 }
